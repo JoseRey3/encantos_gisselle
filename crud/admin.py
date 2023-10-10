d39f25844210608886358897f219b41ce4ff4b38
JoseRey3 <109982070+JoseRey3@users.noreply.github.com>
2023-10-10 15:21:27 -0600

    CRUD
    cambios en la visualizacion de los crud

--- crud/admin.py
+++ crud/admin.py
@@ -4,8 +4,14 @@ from .models import producto, empleado
 # Register your models here.
 class productoadmin(admin.ModelAdmin):
     readonly_fields = ("fecha_creacion",)
+    list_display =("tipo_prenda", "talla", "genero",)
+    search_fields = ("tipo_prenda", "talla",)
+    list_per_page = 10
 admin.site.register(producto, productoadmin)
 
 class empleadoadmin(admin.ModelAdmin):
     readonly_fields = ("fecha_creacion",)
+    list_display =("nombre", "apellido", "cargo",)
+    search_fields = ("nombre", "apellido", "cargo",)
+    list_per_page = 10
 admin.site.register(empleado, empleadoadmin)
