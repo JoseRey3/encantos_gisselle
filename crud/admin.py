d0cbf390d3a5bd31e82d95a68a2fb33e6608e3b0
JoseRey3 <109982070+JoseRey3@users.noreply.github.com>
2023-10-17 07:56:55 -0600

    cambios
    cambios en las migraciones y agregando la tabla categoria

--- crud/admin.py
+++ crud/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from .models import producto, empleado, compra, detalle_compra
+from .models import producto, empleado, compra, detalle_compra, categoria
 
 # Register your models here.
+class categoriaadmin(admin.ModelAdmin):
+    readonly_fields = ("fecha_creacion",)
+    list_display =("nombre_categoria",)
+    search_fields = ("nombre_categoria",)
+    list_per_page = 10
+admin.site.register(categoria, categoriaadmin)
+
 class productoadmin(admin.ModelAdmin):
     readonly_fields = ("fecha_creacion",)
-    list_display =("tipo_prenda", "talla", "genero",)
+    list_display =("tipo_prenda", "talla", "precio_compra","precio_venta", "existencias", )
     search_fields = ("tipo_prenda", "talla",)
     list_per_page = 10
 admin.site.register(producto, productoadmin)
--- crud/models.py
+++ crud/models.py
@@ -1,22 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class categoria(models.Model):
+    nombre_categoria = models.CharField(max_length=50)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __srt__(self):
+        return self.nombre_categoria 
+    
 class producto(models.Model):
     tipo_prenda = models.CharField(max_length=100)
     talla = models.CharField(max_length=4)
-    color = models.CharField(max_length=12)
+    color = models.CharField(max_length=12, blank=True)
     material = models.TextField(blank=True)
+    nombre_categoria = models.ForeignKey(categoria, on_delete=models.PROTECT)
     genero = models.CharField(max_length=12)
-    precio_venta = models.FloatField(max_length=6)
-    marca = models.CharField(max_length=50)
-    cantidad = models.IntegerField()
+    marca = models.CharField(max_length=50, blank=True)
+    precio_compra = models.FloatField(max_length=6, null=True, blank=True, default=0)
+    precio_venta = models.FloatField(max_length=6, null=True, blank=True, default=0)
+    existencias = models.IntegerField(null=True, blank=True, default=0) 
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     usuario = models.ForeignKey(User, on_delete=models.PROTECT)
     
     def __str__(self):
         return self.tipo_prenda
-
+        
 
 class empleado(models.Model):
     nombre = models.CharField(max_length=100)
@@ -43,10 +55,11 @@ class compra(models.Model):
 
 class detalle_compra(models.Model):
     tipo_prenda = models.ForeignKey(producto, on_delete=models.PROTECT)
-    cantidad = models.FloatField(max_length=6)
-    precio_compra = models.FloatField(max_length=6)
-    subtotal= models.FloatField(max_length=6)
+    precio_compra = models.FloatField(max_length=6, null=True, blank=True, default=0)
+    cantidad = models.IntegerField(default=0)
+    subtotal= models.FloatField(max_length=6, null=True, blank=True, default=0)
     numero_compra = models.ForeignKey(compra, on_delete=models.CASCADE)
     
     def __srt__(self):
-        return self.tipo_prenda
+        return self.tipo_prenda 
+    
